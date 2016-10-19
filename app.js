8b8e4b2dbadb1f4c705ef6fe8ce05e4a3f5a68d6
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-10-19 17:54:27 -0400

    added env port

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-var server = require('./index.js').start({port: 3000, static_directory: __dirname + '/public'});
+var server = require('./index.js').start({port: process.env.PORT || 8080, static_directory: __dirname + '/public'});
